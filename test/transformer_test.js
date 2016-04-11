66f8588e5363497eddeddd9182c4c91fa060f557
Philip A. Hirt <phirt13@gmail.com>
2016-04-10 17:13:22 -0700

    finished inversion fucntion

--- test/transformer_test.js
+++ test/transformer_test.js
@@ -14,7 +14,7 @@ describe('blue color transform function', () => {
   it('should transform the bitmap to use blue color scale', () => {
     var blue = fs.readFileSync(__dirname + '/../img/blue.bmp');
     for(var i = 54; i < bitmapData.pixelArrayStart; i += 4) {
-      expect(blue.readUInt8(i)).to.equal(255);
+      expect(blue.readUInt8(i)).to.eql(255);
     }
   });
 });
@@ -23,7 +23,16 @@ describe('green color transform function', () => {
   it('should transform the bitmap to use green color scale ie starting at 55 and then every 4', () => {
     var green = fs.readFileSync(__dirname + '/../img/green.bmp');
     for(var i = 54; i < bitmapData.pixelArrayStart; i += 4) {
-      expect(green.readUInt8(i + 1)).to.equal(255);
+      expect(green.readUInt8(i + 1)).to.eql(255);
+    }
+  });
+});
+
+describe('red color transform function', () => {
+  it('should transform the bitmap to use red color scale (ie starting at 56 and then every 4)', () => {
+    var red = fs.readFileSync(__dirname + '/../img/red.bmp');
+    for(var i = 54; i < bitmapData.pixelArrayStart; i += 4) {
+      expect(red.readUInt8(i + 2)).to.eql(255);
     }
   });
 });
